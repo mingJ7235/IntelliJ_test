0cda8fd439045559b11ea198da78edbef49c99b7
김민재 <joshuara7235@gmail.com>
2021-06-08 21:19:06 +0900

    Test for AWS (using gradle)

---
+++ src/main/java/com/joshua/intellij/Application.java
@@ -0,0 +1,11 @@
+package com.joshua.intellij;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
